81bcdc3ca84e225f2b5204db7240e57b58cbb009
igwevsiggy15 <53847461+igwevsiggy15@users.noreply.github.com>
2019-08-07 14:49:59 -0400

    Add files via upload

---
+++ assign1.java
@@ -0,0 +1,18 @@
+package assign1;
+
+public class assign1 {
+	
+	private static int sum;
+
+	public static void main(String[] args)  
+	{  
+	int a=10,b=5;  
+	while(b>0){  
+	a++;  
+	b--;  
+	}  
+	System.out.println(a);  
+	} 
+	}  
+
+
